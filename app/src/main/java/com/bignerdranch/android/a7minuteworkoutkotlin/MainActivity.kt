5feee81df1744e2f107b2a61e32b739e427c6ae8
Djumabaevs <djumabaevb@gmail.com>
2021-05-10 20:41:13 +0600

    button works and binding set

--- app/src/main/java/com/bignerdranch/android/a7minuteworkoutkotlin/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/a7minuteworkoutkotlin/MainActivity.kt
@@ -2,10 +2,21 @@ package com.bignerdranch.android.a7minuteworkoutkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.bignerdranch.android.a7minuteworkoutkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.llStart.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Let's start the exercise!", Toast.LENGTH_SHORT).show()
+
+        }
+
     }
 }
