409e46b55cf1d9c4b421708271b2d4566e725d8f
Djumabaevs <djumabaevb@gmail.com>
2021-05-12 11:09:58 +0600

    supportActionBar

--- app/src/main/java/com/bignerdranch/android/a7minuteworkoutkotlin/ExerciseActivity.kt
+++ app/src/main/java/com/bignerdranch/android/a7minuteworkoutkotlin/ExerciseActivity.kt
@@ -2,10 +2,18 @@ package com.bignerdranch.android.a7minuteworkoutkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bignerdranch.android.a7minuteworkoutkotlin.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
+    private lateinit var be: ActivityExerciseBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        be = ActivityExerciseBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_exercise)
+        setContentView(be.root)
+
+        setSupportActionBar(be.toolbarExerciseActivity)
+        val actionbar = supportActionBar
+
     }
 }
