903b384a2fdd288167663f7e2ed46b477b420247
Djumabaevs <djumabaevb@gmail.com>
2021-05-16 13:34:18 +0600

    countDown timer

--- app/src/main/java/com/bignerdranch/android/a7minuteworkoutkotlin/ExerciseActivity.kt
+++ app/src/main/java/com/bignerdranch/android/a7minuteworkoutkotlin/ExerciseActivity.kt
@@ -2,10 +2,12 @@ package com.bignerdranch.android.a7minuteworkoutkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import com.bignerdranch.android.a7minuteworkoutkotlin.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
     private lateinit var be: ActivityExerciseBinding
+    private var restTimer: CountDownTimer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         be = ActivityExerciseBinding.inflate(layoutInflater)
