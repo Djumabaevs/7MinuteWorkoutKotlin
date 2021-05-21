0705ee43a5eedb4089daac56894f29ed685c7a55
Djumabaevs <djumabaevb@gmail.com>
2021-05-21 13:48:49 +0600

    add activity finish

---
+++ app/src/main/java/com/bignerdranch/android/a7minuteworkoutkotlin/FinishActivity.kt
@@ -0,0 +1,17 @@
+package com.bignerdranch.android.a7minuteworkoutkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bignerdranch.android.a7minuteworkoutkotlin.databinding.ActivityFinishBinding
+
+class FinishActivity : AppCompatActivity() {
+    private lateinit var fe: ActivityFinishBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        fe = ActivityFinishBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(fe.root)
+
+
+    }
+}
