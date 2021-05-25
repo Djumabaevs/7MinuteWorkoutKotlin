baf0468163d5f1c47cdf7b4f2d1eaaa47d32dacb
Djumabaevs <djumabaevb@gmail.com>
2021-05-25 13:40:40 +0600

    add factors to class

--- app/src/main/java/com/bignerdranch/android/a7minuteworkoutkotlin/SqliteOpenHelper.kt
+++ app/src/main/java/com/bignerdranch/android/a7minuteworkoutkotlin/SqliteOpenHelper.kt
@@ -1,6 +1,11 @@
 package com.bignerdranch.android.a7minuteworkoutkotlin
 
-class SqliteOpenHelper: SqliteOpenHelper {
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class SqliteOpenHelper(context: Context, factory: SQLiteDatabase.CursorFactory?):
+    SQLiteOpenHelper(context, DATABASE_NAME, factory, DATABASE_VERSION) {
 
     companion object {
         private const val DATABASE_VERSION = 1 // This DATABASE Version
@@ -8,5 +13,13 @@ class SqliteOpenHelper: SqliteOpenHelper {
         private const val TABLE_HISTORY = "history" // Table Name
         private const val COLUMN_ID = "_id" // Column Id
         private const val COLUMN_COMPLETED_DATE = "completed_date" // Column for Completed Date
+    }
+
+    override fun onCreate(db: SQLiteDatabase?) {
+
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+
     }
 }
